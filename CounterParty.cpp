0491f8fd44507745f8a82964e4984a6b840f76e1
Chengbin Wang <wangchengbin12@gmail.com>
2015-03-02 19:59:18 -0500

    Counterparty changes

--- CounterParty.cpp
+++ CounterParty.cpp
@@ -8,6 +8,9 @@
 #include <time.h>
 #include <stdio.h>
 #include <string.h>
+#include <thrust/device_vector.h>
+#include "Bank.cpp"
+#include "Swap.cpp"
 
 
 using namespace std;
@@ -18,26 +21,23 @@ class CounterParty{
 public:
 
 	string id;
-	string rating;
 	float hazard_rate;
 	//int position; //1 represents long, -1 represents short to the counterparty
+	thrust::device_vector<Deal> deals;
 
+	CounterParty(){
+       this->id="";
+       this->hazard_rate=0;
+	}
 
-	CounterParty(string id,string rating){
+	CounterParty(string id){
+	       this->id=id;
+	       this->hazard_rate=0;
+		}
+
+	CounterParty(string id,string rate){
        this->id=id;
-       if(boost::iequals("A",rating)){
-          hazard_rate=0.02;
-       }else if(boost::iequals("B",rating)){
-    	   hazard_rate=0.04;
-       }else if(boost::iequals("C",rating)){
-    	   hazard_rate=0.06;
-       }else if(boost::iequals("D",rating)){
-    	   hazard_rate=0.08;
-       }else if(boost::iequals("E",rating)){
-    	   hazard_rate=0.1;
-       }
-
-       //this->position=atoi(position.c_str());
+       this->hazard_rate=stof(rate);
 	}
 
 
