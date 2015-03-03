00f35e76a51284288d2d90f9dbc5c92a73147ec2
Chengbin Wang <wangchengbin12@gmail.com>
2015-03-02 20:00:01 -0500

    add new main cpp

---
+++ HW2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "CounterParty.cpp";
+using namespace std;
+
+int main() {
+
+	//	Bank citi;
+	//	citi.createCounterParties("CounterParty.txt");
+	//	citi.createFX("FX.txt");
+	//	citi.createSwap("Swap.txt");
+	//	citi.createMapping("DealMap.txt");
+
+//	Curve curve('U');
+//	cout<<curve.getYield(0.5)<<endl;
+	//cout<<"hello"<<endl;
+
+
+
+}
